94ff7d4ad38a2c19bf8ddb236f0460642f6d0138
DavidHernandezMorones <david.2.hernandez@atos.net>
2022-07-11 17:30:43 -0500

    Script SQL para crear las tablas

---
+++ create_tables.sql
@@ -0,0 +1,41 @@
+USE campus_practice_sql;
+
+CREATE TABLE Company
+(
+    Id      INT PRIMARY KEY IDENTITY (1, 1),
+    Name    VARCHAR(150) UNIQUE,
+    Address VARCHAR(MAX)
+);
+
+CREATE TABLE Status
+(
+    Id   INT PRIMARY KEY IDENTITY (1, 1),
+    Name VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE Projects
+(
+    Id         INT PRIMARY KEY IDENTITY (1, 1),
+    Name       VARCHAR(150) UNIQUE,
+    StartDate  DATE,
+    Deadline   DATE,
+    FinishedOn DATE,
+    StatusId   INT
+        CONSTRAINT FK_Projects_Status REFERENCES Status (Id)
+);
+
+CREATE TABLE Employees
+(
+    Id        INT PRIMARY KEY IDENTITY (1, 1),
+    FirstName VARCHAR(50),
+    LastName  VARCHAR(50),
+    Email     VARCHAR(100) UNIQUE,
+    Phone     VARCHAR(12) UNIQUE,
+    Salary    FLOAT
+);
+
+CREATE TABLE Projects_Employees(
+    Id INT PRIMARY KEY IDENTITY (1, 1),
+    ProjectId INT,
+    EmployeesId INT
+);
